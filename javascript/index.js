6dde85985063298dd2de296f5b00746e1ebd4d2f
Dylan Nguyen <dylannguyen@MacBook-Air.local>
2021-01-29 20:21:18 -0800

    Scrapped project:(

---
+++ javascript/index.js
@@ -0,0 +1,50 @@
+console.log("Hi");
+
+
+/*
+
+Final Grade Calculator:
+We need to take the Mastery Percentage and multiply it by 100
+Take that answer and multiply that by Mastery Weight%
+
+Example: 85% of Mastery -> 85 -> multiply by 90% weight and that should get 76.5% of final grade
+
+Repeat for College Readiness
+
+Then add both up and you'll get final grade.
+
+Do Desired grade - final grade???
+
+Take with grain of salt:
+Required = (Goal − Current × (100% − Final Weight)) / Final Weight
+
+Grade = Mastery(90%) + CR(10%)
+
+D = M*mw + C*cw
+
+******   M = D/mw - C*cr/mw    Something's missing here  *******
+
+
+98.3 percent on huan's final
+98.3*0.9 = 88.47 + 11.5 = 99.97
+
+We need a way to bump up the 85.13 score on mastery to a 98.3 score on Mastery
+
+63/74 -> 98.3
+
+
+
+35/26 on the test means you need a 134.6% to raise your grade to 100%
+dklhvakjlsdfhlkjshfalkjsfhalkjsdfhas SCRAPPED!!!!
+
+
+RIP our brains
+
+85.13*90% = 76.617
+115*10% = 11.5
+76.617+11.5 = 88.117 -> Pretty Close to Huan's grade
+
+—————————————————————————————————————————————————————————————
+
+
+*/
